9bd5daa0b8923543b28be212fc9bffc2541bdc4e
Gmaneesee <36547067+Gmaneesee@users.noreply.github.com>
2018-02-18 11:18:58 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -9,6 +9,10 @@ client.on('message', message => {
     if (message.content === 'Hey jake, what was your merch link again?') {
     	message.reply('fanjoy.co/jakepaul, next time remember it you stupid cunt nugget bitch ass fuck');
   	}
+client.on('message', message => {
+    if (message.content === 'Its Everyday Bro sucks') {
+    	message.reply('Well guess what, your life sucks too BITCH');
+  	}
 });
 
 // THIS  MUST  BE  THIS  WAY
